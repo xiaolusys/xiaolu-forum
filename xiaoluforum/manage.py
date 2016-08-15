41a81a00b8971ac4863bfadb4f6562033d2336cf
imeron <xiuqing.mei@xiaolu.so>
2016-08-15 23:27:25 +0800

    fix manage and .drone.yml

--- xiaoluforum/manage.py
+++ xiaoluforum/manage.py
@@ -4,8 +4,14 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.test")
+    if os.environ.get('TARGET') in ('staging',):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.staging")
+
+    elif os.environ.get('TARGET') in ('production',):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "product.settings.production")
+
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.test")
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
