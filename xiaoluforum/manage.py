1ca838017b182740cfba1fdfb2b7df7bf85cb23a
hui.deng3 <hlkysf@163.com>
2016-08-18 17:50:29 +0800

    jiem

--- xiaoluforum/manage.py
+++ xiaoluforum/manage.py
@@ -4,12 +4,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.staging")
-=======
     if os.environ.get('TARGET') in ('staging',):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.staging")
->>>>>>> 2d2f41afb59d73a0458889f290239efbc8ebb489
 
     elif os.environ.get('TARGET') in ('production',):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "product.settings.production")
