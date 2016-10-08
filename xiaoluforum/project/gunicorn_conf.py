17e25cded6ac7352babd56ddf532a0f6f26bbde1
imeron <xiuqing.mei@xiaolu.so>
2016-10-08 16:27:38 +0800

    add more worker

--- xiaoluforum/project/gunicorn_conf.py
+++ xiaoluforum/project/gunicorn_conf.py
@@ -4,18 +4,18 @@
 
 import multiprocessing
 
-workers = 2
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:9000"
 
 timeout = 20
-worker_connections = 500  # all worker handle max connects a time
+worker_connections = multiprocessing.cpu_count() * 200  # all worker handle max connects a time
 # The maximum number of requests a worker will process before restarting.
 # This is a simple method to help limit the damage of memory leaks
-max_requests = 800
+max_requests = 2000
 # The maximum number of pending connections.
 # Exceeding this number results in the client getting an error when attempting to connect.
 # It should only affect servers under significant load.
-backlog = 400
+backlog = multiprocessing.cpu_count() * 200
 
 # accesslog='/var/log/taobao/gunicorn.out'
 # access_log_format="%(h)s %(l)s %(t)s %(r)s %(l)s %(s)s %(l)s %(b)s %(l)s %(D)s "
